09aa1621271fae0b93487b95aab6bf36081014c7
Dan <daniel_palma@yahoo.com>
2017-06-30 12:18:23 -0400

    Basic toggling open/closed

--- src/timepicker.js
+++ src/timepicker.js
@@ -9,12 +9,41 @@ export default class TimePicker extends Component {
         this.state = {
             isOpen: false
         }
+        this.showDropdown = this.showDropdown.bind(this);
+        this.hideDropdown = this.hideDropdown.bind(this);
+        this.toggleDropdown = this.toggleDropdown.bind(this);
+    }
+
+    showDropdown() {
+        this.setState({ isOpen: true });
+        document.addEventListener("click", this.hideDropdown);
+    }
+        
+    hideDropdown() {
+        this.setState({ isOpen: false });
+        document.removeEventListener("click", this.hideDropdown);
+    }
+
+    toggleDropdown() {
+        if (this.state.isOpen) {
+            this.hideDropdown();
+        } else {
+            this.showDropdown();
+        }
     }
 
     render() {
         return (
-            <div className="timepicker">
-                <div className="timepicker__input">TimePicker</div>
+            <div className={"timepicker__container" + (this.state.isOpen ? " timepicker__container__open" : " timepicker__container__closed")}>
+                <div className="timepicker__display" onClick={this.toggleDropdown}>TimePicker</div>
+                <div className="timepicker__droplist">
+                    <ul>
+                        <li>12:00</li>
+                        <li>1:00</li>
+                        <li>2:00</li>
+                        <li>3:00</li>
+                    </ul>
+                </div>
             </div>
         )
     }
