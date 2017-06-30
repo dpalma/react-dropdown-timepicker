dd1b9c3f3922e53a0426b2e16d20852d67a257a9
Dan <daniel_palma@yahoo.com>
2017-06-29 23:11:56 -0400

    Initial webpack setup

---
+++ example/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import Example from './example'
+
+render(
+    <Example />,
+    document.getElementById("root")
+)
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require("webpack")
+const path = require("path")
+
+module.exports = {
+	entry: [
+		'./example/index.js',
+	],
+
+	output: {
+		path: path.join(__dirname, '/example'),
+		filename: './bundle.js',
+		publicPath: 'example/'
+	},
+
+	module: {
+		loaders: [
+			{ test: /\.js$/, exclude: [/node_modules/], loaders: ['babel-loader'] },
+			{ test: /\.jsx$/, exclude: [/node_modules/], loaders: ['babel-loader'] }
+        ]
+    }
+    
+    
+}
