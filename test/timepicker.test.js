49c885642852e3fa7c83c55b10380e9e772d3ba9
Dan <daniel_palma@yahoo.com>
2017-07-07 11:07:34 -0400

    Moved initialization tests to their own block

--- test/timepicker.test.js
+++ test/timepicker.test.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, shallow } from 'enzyme';
 
 import TimePicker from '../src/timepicker';
 
 describe("TimePicker component", ()=>{
-  test("default value is 12", () => {
-    const tp = shallow(<TimePicker />);
-    expect(tp.find(".timepicker__display").text()).toEqual("12:00");
+
+  describe("initialization", ()=>{
+    test("default value is 12", () => {
+      const tp = render(<TimePicker />);
+      expect(tp.find(".timepicker__display").text()).toEqual("12:00");
+    })
+
+    test("can handle a Date object input", () => {
+      const t = new Date(2000,1,1,3,32,0);
+      const tp = render(<TimePicker time={t} />);
+      expect(tp.find(".timepicker__display").text()).toEqual("3:32");
+    })
+
+    test("can handle a simple object input", () => {
+      const t = {hour:4,minute:44};
+      const tp = render(<TimePicker time={t} />);
+      expect(tp.find(".timepicker__display").text()).toEqual("4:44");
+    })
   })
 
-  test("can handle a Date object input", () => {
-    const t = new Date(2000,1,1,3,32,0);
-    const tp = shallow(<TimePicker time={t} />);
-    expect(tp.find(".timepicker__display").text()).toEqual("3:32");
-  })
+  describe("selection", ()=>{
 
-  test("can handle a simple object input", () => {
-    const t = {hour:4,minute:44};
-    const tp = shallow(<TimePicker time={t} />);
-    expect(tp.find(".timepicker__display").text()).toEqual("4:44");
   })
 
 })
