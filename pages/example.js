4536ad4769f123b71815e54094a2f10feccf8875
Dan <daniel_palma@yahoo.com>
2017-07-20 19:55:28 -0400

    Added separate 12- and 24-hour format examples
    Also formatted property documentation as a table

--- pages/example.js
+++ pages/example.js
@@ -9,23 +9,37 @@ export default class Example extends Component {
     super(props);
     let now = new Date();
     this.state = {
-      hour: now.getHours(),
-      minute: now.getMinutes()
+      time12hr: {
+        hour: now.getHours(),
+        minute: now.getMinutes()
+      },
+      time24hr: {
+        hour: now.getHours(),
+        minute: now.getMinutes()
+      }
     }
   }
 
-  handleTimeChange(time) {
-    this.setState(time);
+  handleTime24Change(time) {
+    this.setState({time24hr:time});
+  }
+
+  handleTime12Change(time) {
+    this.setState({time12hr:time});
   }
 
   render() {
-    const { hour, minute } = this.state;
+    const { time12hr, time24hr } = this.state;
     return(
       <div>
-        <div>
-          {"Selected "+hour+":"+padStart(minute, 2, "0")}
+        <div className="example__24hrcontainer">
+          <span className="example__24hrtext">{"Selected "+time24hr.hour+":"+padStart(time24hr.minute, 2, "0")}</span>
+          <TimePicker time={this.state.time24hr} onChange={this.handleTime24Change.bind(this)}/>
         </div>
-        <TimePicker time={this.state} onChange={this.handleTimeChange.bind(this)}/>
+        <div className="example__12hrcontainer">
+          <span className="example__12hrtext">{"Selected "+time12hr.hour+":"+padStart(time12hr.minute, 2, "0")}</span>
+          <TimePicker time={this.state.time12hr} onChange={this.handleTime12Change.bind(this)} displayFormat="12-hour" />
+        </div>
       </div>
     )
   }
